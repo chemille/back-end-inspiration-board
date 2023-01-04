90543c4e1d98b06130ee20eca77ebc5d1a663eaf
ashanti <ammanier2@gmail.com>
2023-01-04 12:52:04 -0500

    added POST route for board model

--- app/models/board.py
+++ app/models/board.py
@@ -4,3 +4,11 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner= db.Column(db.String)
+
+
+    # def to_dict(self):
+    #     return {
+    #         "id": self.board_id,
+    #         "title": self.title,
+    #         "owner": self.owner
+    #     }
--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -4,3 +4,26 @@ from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
 board_bp = Blueprint("board", __name__, url_prefix="/board")
+
+# Create a New Board -> post method
+
+@board_bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json()
+    
+    try:
+        new_board = Board(
+            # board_id=request_body["board_id"],
+            title=request_body["title"],
+            owner=request_body["owner"]
+        )
+    except KeyError:
+        return {"details": "Missing Data"}, 400
+
+    db.session.add(new_board)
+    db.session.commit()
+
+    return make_response(f"Board {new_board.title} has been successfully created", 201)
+    # we also can send back the entire dictionary (new board) as confirmation to the client
+
+    
