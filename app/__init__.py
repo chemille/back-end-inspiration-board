16ce85007793eb8ec47536a358f0ff0a6b6debc4
ashanti <ammanier2@gmail.com>
2023-01-04 13:42:52 -0500

    changed board POST route to return new board dict

--- app/__init__.py
+++ app/__init__.py
@@ -29,8 +29,8 @@ def create_app():
     # Register Blueprints here
     # from .routes import example_bp
     # app.register_blueprint(example_bp)
-    from .routes.board_routes import board_bp
-    app.register_blueprint(board_bp)
+    from .routes.board_routes import boards_bp
+    app.register_blueprint(boards_bp)
     
     CORS(app)
     return app
--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -3,17 +3,16 @@ from app import db
 from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
-board_bp = Blueprint("board", __name__, url_prefix="/board")
+boards_bp = Blueprint("board", __name__, url_prefix="/boards")
 
 # Create a New Board -> post method
 
-@board_bp.route("", methods=["POST"])
+@boards_bp.route("", methods=["POST"])
 def create_board():
     request_body = request.get_json()
     
     try:
         new_board = Board(
-            # board_id=request_body["board_id"],
             title=request_body["title"],
             owner=request_body["owner"]
         )
@@ -23,7 +22,12 @@ def create_board():
     db.session.add(new_board)
     db.session.commit()
 
-    return make_response(f"Board {new_board.title} has been successfully created", 201)
+    # return make_response(f"Board {new_board.title} has been successfully created", 201)
     # we also can send back the entire dictionary (new board) as confirmation to the client
-
-    
+    return {
+        "board" : {
+            "id": new_board.board_id,
+            "title": new_board.title,
+            "owner": new_board.owner
+        }
+    }, 201
