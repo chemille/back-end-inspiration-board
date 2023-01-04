e936bea8eeb9fd4a07cac77024ff8af74d7e8bd9
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2023-01-04 08:52:33 -0800

    added route files for each model. Created and registered blueprint for board_route

--- app/__init__.py
+++ app/__init__.py
@@ -20,15 +20,17 @@ def create_app():
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
 
+    from app.models.board import Board
+    from app.models.card import Card
+
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
     # from .routes import example_bp
     # app.register_blueprint(example_bp)
-
-    from app.models.board import Board
-    from app.models.card import Card
+    from .routes.board_routes import board_bp
+    app.register_blueprint(board_bp)
     
     CORS(app)
     return app
--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.board import Board
 
 # example_bp = Blueprint('example_bp', __name__)
+board_bp = Blueprint("board", __name__, url_prefix="/board")
