18ae382d313063bae5a3914131505f946a1440f0
ashanti <ammanier2@gmail.com>
2023-01-06 10:41:50 -0500

    model comments for migration push

--- app/models/board.py
+++ app/models/board.py
@@ -5,7 +5,7 @@ class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     owner = db.Column(db.String)
-    # cards = db.relationship("Card", back_populates="board")
+    cards = db.relationship("Card", back_populates="board")
 
     def to_dict(self):
         return {
