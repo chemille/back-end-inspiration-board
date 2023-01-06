b0dec6a7d9229edd82aac8d05e5a0688d05c132a
Lourdes Zaragoza <lourdes.zaragoza34@gmail.com>
2023-01-06 07:49:41 -0800

    trying to send out migrations folder

--- app/models/board.py
+++ app/models/board.py
@@ -3,7 +3,7 @@ from app import db
 # Parent Model
 class Board(db.Model):
     board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
+    # title = db.Column(db.String)
     owner = db.Column(db.String)
     cards = db.relationship("Card", back_populates="board")
 
